9cedd537f4670579669aa1a48171a6c14d174f10
Yolosopher <nika.nishnianidze.10@gmail.com>
2020-06-10 17:36:33 +0400

    cat-toggle

--- js/main.js
+++ js/main.js
@@ -1,20 +1,19 @@
 var swiper = new Swiper('.swiper-container', {
-    slidesPerView: 4,
-    direction: 'horizontal',
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-    on: {
-      resize: function () {
-        swiper.changeDirection(getDirection());
-      }
+  slidesPerView: 4,
+  direction: 'horizontal',
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  on: {
+    resize: function () {
+      swiper.changeDirection(getDirection());
     }
-  });
-
-// function getDirection() {
-//     var windowWidth = window.innerWidth;
-//     var direction = window.innerWidth <= 720 ? 'vertical' : 'horizontal';
-
-//     return direction;
-// }
+  }
+});
+let category = document.getElementById('srch-txt');
+let catUl = document.querySelector('.cat-toggle-ul');
+catUl.classList.toggle('hidden');
+category.addEventListener('click', ()=> {
+  catUl.classList.toggle('hidden');
+});
