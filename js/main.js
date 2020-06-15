bd90fe0bd704212ab3e52d5853a9d5f58c82dc98
Yolosopher <61516311+Yolosopher@users.noreply.github.com>
2020-06-15 23:23:26 +0400

    a

--- js/main.js
+++ js/main.js
@@ -13,15 +13,7 @@ var swiper = new Swiper('.banners', {
   }
 });
 // top products slider
-var swiper = new Swiper('.top-products', {
-  slidesPerView: 'auto',
-  direction: 'horizontal',
-  on: {
-    resize: function () {
-      swiper.changeDirection(getDirection());
-    }
-  }
-});
+
 
 
 
@@ -37,10 +29,23 @@ let product = document.getElementById('top-prod-item');
 let productsWidth = product.clientWidth;
 let products = document.querySelectorAll('.top-prod-items');
 let topProdUl = document.querySelector('.top-prod-ul');
+let topProducts = document.querySelector('.top-products');
+
 
 if (productsWidth < 200) {
   for (let item of products) {
     item.classList.add('respo-prod-width')
   }
-  topProdUl.classList.add('respo-prod-ul')
-}
+  topProdUl.classList.add('respo-prod-ul');
+  topProducts.classList.add('top-products-slider')
+};
+
+var swiper = new Swiper('.top-products-slider', {
+  slidesPerView: 'auto',
+  direction: 'horizontal',
+  on: {
+    resize: function () {
+      swiper.changeDirection(getDirection());
+    }
+  }
+});
