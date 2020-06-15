fd904b3263d5ba03ea2094977ac44675e08cdede
Yolosopher <nika.nishnianidze.10@gmail.com>
2020-06-15 17:20:56 +0400

    almost done

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
-var swiper = new Swiper('.swiper-container', {
-  slidesPerView: 4,
+// banners slider
+var swiper = new Swiper('.banners', {
+  slidesPerView: 'auto',
   direction: 'horizontal',
   navigation: {
     nextEl: '.swiper-button-next',
@@ -11,9 +12,35 @@ var swiper = new Swiper('.swiper-container', {
     }
   }
 });
+// top products slider
+var swiper = new Swiper('.top-products', {
+  slidesPerView: 'auto',
+  direction: 'horizontal',
+  on: {
+    resize: function () {
+      swiper.changeDirection(getDirection());
+    }
+  }
+});
+
+
+
 let category = document.getElementById('srch-txt');
 let catUl = document.querySelector('.cat-toggle-ul');
 catUl.classList.toggle('hidden');
 category.addEventListener('click', ()=> {
   catUl.classList.toggle('hidden');
 });
+
+
+let product = document.getElementById('top-prod-item');
+let productsWidth = product.clientWidth;
+let products = document.querySelectorAll('.top-prod-items');
+let topProdUl = document.querySelector('.top-prod-ul');
+
+if (productsWidth < 200) {
+  for (let item of products) {
+    item.classList.add('respo-prod-width')
+  }
+  topProdUl.classList.add('respo-prod-ul')
+}
